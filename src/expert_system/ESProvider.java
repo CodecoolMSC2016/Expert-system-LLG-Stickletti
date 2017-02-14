2f4e63d5235e3b49077b93897f3b1a7ea29f0a90
agnes-kabaly <alicet.woods@gmail.com>
2017-02-14 20:21:50 +0100

    rules.xml is ready

--- src/expert_system/ESProvider.java
+++ src/expert_system/ESProvider.java
@@ -1,5 +1,12 @@
 package expert_system;
 
 public class ESProvider {
+	FactParser factParser;
+	RuleParser ruleParser;
 
+	public ESProvider(FactParser factParser, RuleParser ruleParser) {
+		this.factParser = factParser;
+		this.ruleParser = ruleParser;
+	}
 }
+
