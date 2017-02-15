9a7f9b093ab6204f28ba7d1b1e5d11fb3dd676f9
Jakubinyi <jakubinyi@gmail.com>
2017-02-15 13:52:22 +0100

    Single/Multiple value repactor

--- src/expert_system/MultipleValue.java
+++ src/expert_system/MultipleValue.java
@@ -5,4 +5,10 @@ public class MultipleValue extends Value{
 	public MultipleValue(String param) {
 
 	}
+
+	@Override
+	public String[] getInputPattern() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/expert_system/SingleValue.java
+++ src/expert_system/SingleValue.java
@@ -5,5 +5,11 @@ public class SingleValue extends Value {
 	public SingleValue(String param) {
 
 	}
+
+	@Override
+	public String[] getInputPattern() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
 
