7f8cfad96019d475dce82fe990ad805b0e40d8cf
agnes-kabaly <alicet.woods@gmail.com>
2017-02-16 22:12:06 +0100

    Fact started

--- src/expert_system/Fact.java
+++ src/expert_system/Fact.java
@@ -1,23 +1,31 @@
 package expert_system;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Fact {
 
 	String id;
 	boolean value;
+	String description;
+	Map<String, Boolean> factMap = new HashMap<>();
 
 	public void setFactValueByID(String id, boolean value) {
+
 		this.id = id;
 		this.value = value;
+
+		factMap.put(id, value);
 	}
 
 	public Set<String> getIDSet() {
-		return null;
+
+		return factMap.keySet();
 	}
 
 	public boolean getValueByID() {
-		return false;
+		return factMap.get(id);
 	}
 
 	public String getDescription() {
@@ -25,7 +33,6 @@ public class Fact {
 	}
 
 	public Fact(String description) {
-
+		this.description = description;
 	}
 }
-
